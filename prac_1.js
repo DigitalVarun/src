c2db0a3b187bbb93c259d4deff6957929cd2c30c
Varun <digitalvarun.agency@gmail.com>
2023-12-03 23:12:26 +0530

    New

---
+++ prac_1.js
@@ -0,0 +1,31 @@
+let obj = {
+  name: "Varun",
+  age: 20,
+  adress: "Jaipur"
+}
+
+console.log(obj.adress);
+
+let s = "Varun";
+let a = 20;
+console.log(s + "'s age is " + a + " years old.")
+console.log(typeof (a + s));
+
+const newObj = {
+  name: "Varun",
+  age: 20,
+  dept: "IT",
+  roll: "21EJCIT139"
+}
+newObj.college = "Jecrc";
+console.log(newObj)
+newObj['college'] = "Jecrc"
+
+const wordDic = {
+  Abrogate: "To revoke",
+  Anachronism: "Something out of place for the time period",
+  Arrant: "Entirely and completely",
+  Artless: "Without deception",
+  Asperity: "Harsh in tone"
+}
+console.log(wordDic);
